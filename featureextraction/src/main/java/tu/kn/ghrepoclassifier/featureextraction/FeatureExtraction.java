7006fa81f790aeeb5d078634a5ff6307ad8f0ae8
FelixNeutatz <neutatz@googlemail.com>
2016-11-25 13:03:57 +0100

    fix performance bottlenecks and remove features which take too much time

--- featureextraction/src/main/java/tu/kn/ghrepoclassifier/featureextraction/FeatureExtraction.java
+++ featureextraction/src/main/java/tu/kn/ghrepoclassifier/featureextraction/FeatureExtraction.java
@@ -16,6 +16,8 @@ import java.util.Map;
  * Created by felix on 24.11.16.
  */
 public class FeatureExtraction {
+	
+	
 
 	public static String extractFeatures(String url, GitHub github) throws IOException {
 		String [] splits = url.split("/");
@@ -26,20 +28,34 @@ public class FeatureExtraction {
 		GHRepository repo = github.getRepository(user + "/" + reponame);
 		System.out.println(repo);
 		
+		
+		//the commented features need to much time
+		
 		int numberBranches = repo.getBranches().size(); 					//number of branches
 		int numberForks = repo.getForks(); 									//number of forks
 		int numberOpenIssues = repo.getOpenIssueCount();					//number of open issues
-		int totalNumberIssues = repo.getIssues(GHIssueState.ALL).size();	//number of total issues
+		//int totalNumberIssues = repo.getIssues(GHIssueState.ALL).size();	//number of total issues
 		int repoSize = repo.getSize();										//size of the repository in bytes
 		int numberStars = repo.getStargazersCount();						//number of stars
 		int numberSubscribers = repo.getSubscribersCount();					//number of subscribers
 		int numberWatchers = repo.getWatchers();							//number of watchers
 		int numberReleases = repo.listReleases().asList().size();			//number of releases
-		int numberCommits = repo.listCommits().asList().size();				//number of commits
-		int numberContributors = repo.listContributors().asList().size();	//number of contributors
+		
 		int hasDownloads = repo.hasDownloads() ? 1 : 0;						//was the repo downloaded
+		int hasDescription = (repo.getDescription().length() > 0) ? 1 : 0;	//has a decription
 		int numberProgrammingLanguages = repo.listLanguages().size();		//number of programming languages
 		int hasLicense = repo.getLicense() == null ? 0 : 1;					//repo has a license
+
+
+		int numberContributors = 0;											//number of contributors
+		int numberCommits = 0;												//number of commits
+
+		Iterator<GHRepository.Contributor> i = repo.listContributors().iterator();
+		while(i.hasNext()) {
+			GHRepository.Contributor c = i.next();
+			numberCommits += c.getContributions();
+			numberContributors++;
+		}
 		
 		long readmeSize;													//size of the readme
 		try {
@@ -49,23 +65,16 @@ public class FeatureExtraction {
 			readmeSize = 0;
 		}
 		
-		int numberTeams;													//number of owning organizations
-		try {
-			numberTeams = repo.getTeams().size();
-		} catch (IOException e){
-			numberTeams = 0;
-		}
-		
 		/*
 		//forbidden
 		int numberCollaborators = repo.getCollaborators().size();			//number of collaborators
 		*/
 		
 		
-		return numberBranches					//number of branches
+		return 
+			numberBranches						//number of branches
 			+ "," + numberForks 				//number of branches
 			+ "," + numberOpenIssues			//number of open issues
-			+ "," + totalNumberIssues			//number of total issues
 			+ "," + repoSize					//size of the repository in bytes
 			+ "," + numberStars					//number of stars
 			+ "," + numberSubscribers			//number of subscribers
@@ -74,10 +83,10 @@ public class FeatureExtraction {
 			+ "," + numberCommits				//number of commits
 			+ "," + numberContributors			//number of contributors 
 			+ "," + hasDownloads				//was the repo downloaded
+			+ "," + hasDescription				//has a decription
 			+ "," + numberProgrammingLanguages	//number of programming languages
 			+ "," + hasLicense					//repo has a license
 			+ "," + readmeSize					//size of the readme
-			+ "," + numberTeams 				//number of owning organizations
 			;
 		
 
@@ -93,6 +102,9 @@ public class FeatureExtraction {
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		File file = new File(classLoader.getResource("data/Beispieleingabe1.csv").getFile());
 
+		String featuresSpark = extractFeatures("https://github.com/google/traceur-compiler", github);
+		System.out.println(featuresSpark);
+		
 		Reader in = new FileReader(file);
 		Iterable<CSVRecord> records = CSVFormat.newFormat(' ').parse(in);
 		for (CSVRecord record : records) {

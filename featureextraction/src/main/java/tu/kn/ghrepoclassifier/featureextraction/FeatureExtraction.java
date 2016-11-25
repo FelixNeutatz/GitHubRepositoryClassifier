bb1bb1f64b17ca60171de5818d4f2818e7e03d27
FelixNeutatz <neutatz@googlemail.com>
2016-11-25 19:03:50 +0100

    optimize crawling

--- featureextraction/src/main/java/tu/kn/ghrepoclassifier/featureextraction/FeatureExtraction.java
+++ featureextraction/src/main/java/tu/kn/ghrepoclassifier/featureextraction/FeatureExtraction.java
@@ -3,6 +3,10 @@ package tu.kn.ghrepoclassifier.featureextraction;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.kohsuke.github.*;
+import org.kohsuke.github.extras.OkHttpConnector;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+import com.squareup.okhttp.Cache;
 
 import java.io.File;
 import java.io.FileReader;
@@ -10,25 +14,27 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Map;
 
 /**
  * Created by felix on 24.11.16.
  */
 public class FeatureExtraction {
-	
-	
+
 
 	public static String extractFeatures(String url, GitHub github) throws IOException {
 		String [] splits = url.split("/");
 		System.out.println(Arrays.toString(splits));
 		String user = splits[splits.length - 2];
 		String reponame = splits[splits.length - 1];
-		
+
 		GHRepository repo = github.getRepository(user + "/" + reponame);
 		System.out.println(repo);
 		
-		
+		return extractFeatures(repo);
+	}
+	
+
+	public static String extractFeatures(GHRepository repo) throws IOException {
 		//the commented features need to much time
 		
 		int numberBranches = repo.getBranches().size(); 					//number of branches
@@ -42,7 +48,7 @@ public class FeatureExtraction {
 		int numberReleases = repo.listReleases().asList().size();			//number of releases
 		
 		int hasDownloads = repo.hasDownloads() ? 1 : 0;						//was the repo downloaded
-		int hasDescription = (repo.getDescription().length() > 0) ? 1 : 0;	//has a decription
+		int hasDescription = (repo.getDescription() != null) ? 1 : 0;	//has a decription
 		int numberProgrammingLanguages = repo.listLanguages().size();		//number of programming languages
 		int hasLicense = repo.getLicense() == null ? 0 : 1;					//repo has a license
 
@@ -72,7 +78,7 @@ public class FeatureExtraction {
 		
 		
 		return 
-			numberBranches						//number of branches
+					numberBranches				//number of branches
 			+ "," + numberForks 				//number of branches
 			+ "," + numberOpenIssues			//number of open issues
 			+ "," + repoSize					//size of the repository in bytes
@@ -88,23 +94,21 @@ public class FeatureExtraction {
 			+ "," + hasLicense					//repo has a license
 			+ "," + readmeSize					//size of the readme
 			;
-		
-
-
 	}
 
 	public static void main(String[] args) throws IOException {
 
-		GitHub github = GitHub.connectUsingOAuth("780afd229d60f12f1ce6aad7adc8712690d20268");
+		Cache cache = new Cache(new File("/tmp/ghrepoclassifier"), 1000 * 1024 * 1024); // 1000MB cache
+
+		GitHub github = GitHubBuilder.fromPropertyFile()
+			.withConnector(new OkHttpConnector(new OkUrlFactory(new OkHttpClient().setCache(cache))))
+			.build();
 		
 		System.out.println(github.getMyself());
 
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		File file = new File(classLoader.getResource("data/Beispieleingabe1.csv").getFile());
 
-		String featuresSpark = extractFeatures("https://github.com/google/traceur-compiler", github);
-		System.out.println(featuresSpark);
-		
 		Reader in = new FileReader(file);
 		Iterable<CSVRecord> records = CSVFormat.newFormat(' ').parse(in);
 		for (CSVRecord record : records) {
--- samplegeneration/src/main/java/tu/kn/ghrepoclassifier/samplegeneration/GenerateSamples.java
+++ samplegeneration/src/main/java/tu/kn/ghrepoclassifier/samplegeneration/GenerateSamples.java
@@ -1,11 +1,19 @@
 package tu.kn.ghrepoclassifier.samplegeneration;
 
+import au.com.bytecode.opencsv.CSVWriter;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+import com.squareup.okhttp.Cache;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
 import org.kohsuke.github.PagedSearchIterable;
+import org.kohsuke.github.extras.OkHttpConnector;
+import tu.kn.ghrepoclassifier.featureextraction.FeatureExtraction;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -13,23 +21,56 @@ import java.util.Map;
  */
 public class GenerateSamples {
 
+	public static void extractIterable(PagedSearchIterable<GHRepository> searchResult, File out, String category){
+		CSVWriter writer = null;
+		try {
+			writer = new CSVWriter(new FileWriter(out), ',', CSVWriter.NO_QUOTE_CHARACTER);
+		
+			// feed in your array (or convert your data to an array)
+			String[] entries = {"","", category};
+
+			System.out.println("size: " + searchResult.getTotalCount());
+			
+			int count = 0;
+			for (GHRepository repo : searchResult) {
+				entries[1] = FeatureExtraction.extractFeatures(repo);
+				entries[0] = repo.getHtmlUrl().toString();
+				count++;
+				System.out.println(count + ": " + entries[0] + ", " + entries[1]);
+				writer.writeNext(entries);
+				writer.flush();
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//HOMEWORK
+	public static void generateHomeWork(GitHub github){
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		File file = new File(classLoader.getResource("data/").getFile() + "homework.csv");
+
+		PagedSearchIterable<GHRepository> homeworkRepos = 
+			github.searchRepositories().q("homework").stars("0").forks("0").list().withPageSize(100);
+		
+		extractIterable(homeworkRepos, file, "HW");
+	}
+
 	public static void main(String[] args) throws IOException {
 
-		GitHub github = GitHub.connectUsingOAuth("780afd229d60f12f1ce6aad7adc8712690d20268");
+		Cache cache = new Cache(new File("/tmp/ghrepoclassifier"), 1000 * 1024 * 1024); // 1000MB cache
+
+		GitHub github = GitHubBuilder.fromPropertyFile()
+			.withConnector(new OkHttpConnector(new OkUrlFactory(new OkHttpClient().setCache(cache))))
+			.build();
 		
 		System.out.println(github.getMyself());
 
-		/*
 		//HOMEWORK
-		PagedSearchIterable<GHRepository> homeworkRepos = github.searchRepositories().q("homework").stars("0").forks("0").list();
-		
-		System.out.println("homework repos size: " + homeworkRepos.getTotalCount()); //62401
-		
-		Iterator i = homeworkRepos.iterator();
-		while(i.hasNext()) {
-			System.out.println(i.next());
-		}
-		
+		generateHomeWork(github);
+
 		//DEV
 		Map<String, GHRepository> devRepos = github.getOrganization("apache").getRepositories();
 
@@ -49,46 +90,48 @@ public class GenerateSamples {
 		
 		
 		//DOCS
-		PagedSearchIterable<GHRepository> docsRepos = github.searchRepositories().q("documentation").list();
+		PagedSearchIterable<GHRepository> docsRepos = 
+			github.searchRepositories().q("documentation").list().withPageSize(100);
 		
 		System.out.println("docs repos size: " + docsRepos.getTotalCount()); //30549
 
-		PagedSearchIterable<GHRepository> docsRepos1 = github.searchRepositories().q("documentation").stars(">5").list();
+		PagedSearchIterable<GHRepository> docsRepos1 = 
+			github.searchRepositories().q("documentation").stars(">5").list().withPageSize(100);
 
 		System.out.println("docs repos size: " + docsRepos1.getTotalCount()); //2918
 		
 
-		PagedSearchIterable<GHRepository> docsReposDEU = github.searchRepositories().q("Dokumentation").list();
+		PagedSearchIterable<GHRepository> docsReposDEU = 
+			github.searchRepositories().q("Dokumentation").list().withPageSize(100);
 
 		System.out.println("docs repos size: " + docsReposDEU.getTotalCount()); //259
 
-		PagedSearchIterable<GHRepository> docsRepos2 = github.searchRepositories().q("manual").list();
+		PagedSearchIterable<GHRepository> docsRepos2 = github.searchRepositories().q("manual").list().withPageSize(100);
 
 		System.out.println("docs repos size: " + docsRepos2.getTotalCount()); //9020
 		
 		//WEB
-		PagedSearchIterable<GHRepository> webRepos = github.searchRepositories().q(".github.io").list();
+		PagedSearchIterable<GHRepository> webRepos = 
+			github.searchRepositories().q(".github.io").list().withPageSize(100);
 
 		System.out.println("web repos size: " + webRepos.getTotalCount()); //560623
 
 
 		//DATA
-		PagedSearchIterable<GHRepository> dataRepos = github.searchRepositories().q("data").size(">100000").list();
+		PagedSearchIterable<GHRepository> dataRepos = 
+			github.searchRepositories().q("data").size(">100000").list().withPageSize(100);
 
 		System.out.println("data repos size: " + dataRepos.getTotalCount()); //7065 
 		
 		//EDU
-		PagedSearchIterable<GHRepository> eduRepos = github.searchRepositories().q("lecture").forks(">10").list();
+		PagedSearchIterable<GHRepository> eduRepos = 
+			github.searchRepositories().q("lecture").forks(">10").list().withPageSize(100);
 
 		System.out.println("edu repos size: " + eduRepos.getTotalCount()); //290
 
-		PagedSearchIterable<GHRepository> eduRepos1 = github.searchRepositories().q("tutorial").forks(">10").list();
+		PagedSearchIterable<GHRepository> eduRepos1 = 
+			github.searchRepositories().q("tutorial").forks(">10").list().withPageSize(100);
 
 		System.out.println("edu repos size: " + eduRepos1.getTotalCount()); //2081 */
-		
-		
-		
-		
-
 	}
 }

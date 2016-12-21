283996f1de77ba487db686680a4e398fe010a202
FelixNeutatz <neutatz@googlemail.com>
2016-12-21 02:01:11 +0100

    add visualization for bag of words features

--- model/ml/runBagOfWords.py
+++ model/ml/runBagOfWords.py
@@ -13,6 +13,8 @@ from config import Config
 from sklearn.feature_extraction import text
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+from sklearn.calibration import CalibratedClassifierCV
+import matplotlib.pyplot as plt
 
 
 # http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
@@ -25,7 +27,7 @@ def visualize(y_real, y_pred):
 
 
 def run():
-    category_frames = readNative(Config.get("feature.extraction.output.path"), 177)
+    category_frames = readNative(Config.get("feature.extraction.output.path"), 184)
 
     train_frame, test_frame = split_train_test(category_frames, test_size=0.1)
 
@@ -44,9 +46,10 @@ def run():
 
     pipeline = Pipeline([('vect', CountVectorizer(stop_words=stop_words)),
                          ('tfidf', TfidfTransformer()),
-                         ('clf', SGDClassifier(loss='hinge')),
+                         ('clf', SGDClassifier(loss='log')),
                          ])
 
+    '''
     # uncommenting more parameters will give better exploring power but will
     # increase processing time in a combinatorial way
     parameters = {
@@ -55,7 +58,7 @@ def run():
         'vect__ngram_range':((1, 1), (1, 2)),  # unigrams or bigrams
         # 'tfidf__use_idf': (True, False),
         # 'tfidf__norm': ('l1', 'l2'),
-        'clf__alpha':(0.00001, 0.000001),
+        'clf__alpha':(0.0001, 0.00001, 0.000001),
         'clf__penalty':('l2', 'elasticnet')
         # 'clf__n_iter': (10, 50, 80),
     }
@@ -80,14 +83,76 @@ def run():
     for param_name in sorted(parameters.keys()):
         print("\t%s: %r" % (param_name, best_parameters[param_name]))
 
+     best_parameters = {
+        'vect__max_df': (0.5),
+        # 'vect__max_features': (None, 5000, 10000, 50000),
+        'vect__ngram_range': ((1, 2)),  # unigrams or bigrams
+        # 'tfidf__use_idf': (True, False),
+        # 'tfidf__norm': ('l1', 'l2'),
+        'clf__alpha': (0.00001),
+        'clf__penalty': ('elasticnet')
+        # 'clf__n_iter': (10, 50, 80),
+    }
+    '''
+
+    best_parameters = {
+        'vect__max_df': (0.5),
+        # 'vect__max_features': (None, 5000, 10000, 50000),
+        'vect__ngram_range': ((1, 1)),  # unigrams or bigrams
+        # 'tfidf__use_idf': (True, False),
+        # 'tfidf__norm': ('l1', 'l2'),
+        'clf__alpha': (0.0001),
+        'clf__penalty': ('elasticnet')
+        # 'clf__n_iter': (10, 50, 80),
+    }
 
     pipeline.set_params(**best_parameters)
 
-    X = train_x.A.ravel()
-    y = np.array(train_y).tolist()
+    X_numpy = np.concatenate((train_x, test_x), axis=0)
+    y_numpy = np.concatenate((train_y, test_y), axis=0)
+
+    X = X_numpy.A.ravel()
+    y = np.array(y_numpy).tolist()
 
     clf = pipeline.fit(X, y)
+
+    '''
+    # feature visualization
+    vec = clf.named_steps['vect']
+    vocabulary = vec.vocabulary_
+
+    classifier = clf.named_steps['clf']
+    weights = classifier.coef_
+
+    category_list = {0: "DATA", 1: "EDU", 2: "HW", 3: "DOCS", 4: "DEV", 5: "WEB"}
+
+    k = 10
+    for id in range(0, len(category_list)):
+        max_ids = np.argsort(-weights[id])
+        values = np.sort(-weights[id])
+
+        feature_names = [""] * k
+        feature_weights = [0] * k
+        for key_d, item in vocabulary.iteritems():
+            for i in range(0, k):
+                if max_ids[i] == item:
+                    feature_names[i] = key_d
+                    feature_weights[i] = -values[i]
+
+        ind = np.arange(k)
+        plt.bar(ind, feature_weights)
+        plt.title('top 10 features for ' + category_list[id])
+        plt.xticks(np.arange(k), feature_names)
+        plt.xlabel('features')
+        plt.ylabel('weight')
+        plt.tight_layout()
+        plt.show()
+    '''
+
     y_pred = clf.predict(test_x.A.ravel())
+
+    y_pred_proba = clf.predict_proba(test_x.A.ravel())
+    print y_pred_proba
 
     visualize(test_y, y_pred)
 
@@ -100,8 +165,14 @@ def run():
 
     attachment_a_x, attachment_a_y = split_target_from_data(attachment_a_matrix)
 
-    attachment_a_y_pred = clf.predict(attachment_a_x.A.ravel())
+    attachment_a_y_pred = clf.predict_proba(attachment_a_x.A.ravel())
 
-    visualize(attachment_a_y, attachment_a_y_pred)
+    print attachment_a_y_pred
+
+    attachment_a_ypred_test = np.argmax(attachment_a_y_pred, axis=1)  # choose class with highest probability per sample
+
+    print attachment_a_ypred_test
+
+    visualize(attachment_a_y, attachment_a_ypred_test)
 
 run()

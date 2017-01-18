5dc40690f9f24a51cecb5016b4ad4936b95b1a66
Kevin Kepp <dev@kevinkepp.de>
2017-01-18 12:13:47 +0100

    Include labeled filtering to runSVM

--- model/ml/runSVM.py
+++ model/ml/runSVM.py
@@ -15,8 +15,9 @@ from config import Config
 
 def run():
     category_frames = read(Config.get("feature.extraction.output.path"), 2000)
+    category_frames = filter_frames(category_frames, get_labeled_data_filter("../../mturk/our_labels/labels.csv"))
 
-    train_frame, test_frame = split_train_test(category_frames, test_size=0.1)
+    train_frame, test_frame = split_train_test(category_frames, test_size=0.2)
 
     mask = np.asarray(np.ones((1, train_frame.shape[1]), dtype=bool))[0]
     mask[0] = False
@@ -40,9 +41,8 @@ def run():
     cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
     grid = GridSearchCV(SVC(decision_function_shape='ovo'), param_grid=param_grid, cv=cv, verbose=10, n_jobs=4)
     grid.fit(train_x, train_y)
-    '''
 
-    C_range = np.logspace(-2, 10, 13)
+    C_range = np.logspace(-4, 4, 9)
     param_grid = dict(estimator__C=C_range)
     cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
     grid = GridSearchCV(OneVsRestClassifier(SVC(kernel="linear")), param_grid=param_grid, cv=cv, verbose=10, n_jobs=4)
@@ -51,10 +51,10 @@ def run():
     print("The best parameters are %s with a score of %0.2f"
           % (grid.best_params_, grid.best_score_))
 
+    clf = grid.best_estimator_
     '''
 
-    clf = SVC(decision_function_shape='ovo', C=1)
-    #clf = LinearSVC(multi_class='ovr', C=10)
+    clf = OneVsRestClassifier(SVC(kernel="linear", C=.01))
     clf.fit(train_x, train_y)
 
     y_train_pred = clf.predict(train_x)
@@ -62,14 +62,10 @@ def run():
     print confusion_matrix(train_y, y_train_pred)
     print f1_score(train_y, y_train_pred, average='weighted')
 
-
     y_test_pred = clf.predict(test_x)
 
     print confusion_matrix(test_y, y_test_pred)
     print f1_score(test_y, y_test_pred, average='weighted')
-
-    '''
-
 
 
 run()
